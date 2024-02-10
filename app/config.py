0d022dbe968eaea18153db5152827b578d994a94
rachhtord <rachael.tordoff@hotmail.co.uk>
2024-02-10 17:14:32 +0000

    fix

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 SECRET_KEY = os.environ['SECRET_KEY']
 ALGORITHM = os.environ['ALGORITHM']
@@ -10,7 +11,7 @@ LOGIN_API_ENDPOINT = os.environ['LOGIN_API_ENDPOINT']
 
 
 SQLALCHEMY_USER = os.environ['SQLALCHEMY_USER']
-SQLALCHEMY_PASSWORD = os.environ['SQLALCHEMY_PASSWORD']
+SQLALCHEMY_PASSWORD = urllib.parse.quote_plus(os.environ['SQLALCHEMY_PASSWORD']).replace('%40', '%%40')
 SQLALCHEMY_HOST = os.environ['SQLALCHEMY_HOST']
 SQLALCHEMY_PORT = os.environ['SQLALCHEMY_PORT']
 SQLALCHEMY_DB = os.environ['SQLALCHEMY_DB']
